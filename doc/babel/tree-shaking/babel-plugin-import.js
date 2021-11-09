da051681e1984d8232d7f58001f8dbc156f3415e
wang.haoyu <228233464@qq.com>
2021-11-09 22:39:06 +0800

    feat(babel): import插件思路注释

--- doc/babel/tree-shaking/babel-plugin-import.js
+++ doc/babel/tree-shaking/babel-plugin-import.js
@@ -1,24 +1,31 @@
-const core = require('@babel/core');
 const t = require('@babel/types');
 
 function babelPluginImport(options) {
   const { libraryName = 'hy-store' } = options;
   return {
     visitor: {
+      // 匹配ImportDeclaration时进入
       ImportDeclaration(nodePath) {
+        // checked Validity
         if (checkedDefaultImport(nodePath) || checkedLibraryName(nodePath)) {
           return;
         }
         const node = nodePath.node;
+        // 获取声明说明符
         const { specifiers } = node;
+        // 遍历对应的声明符
         const importDeclarations = specifiers.map((specifier, index) => {
+          // 获得原本导入的模块
           const moduleName = specifier.imported.name;
+          // 获得导入时的重新命名
           const localIdentifier = specifier.local;
           return generateImportStatement(moduleName, localIdentifier);
         });
         if (importDeclarations.length === 1) {
+          // 如果仅仅只有一个语句时
           nodePath.replaceWith(importDeclarations[0]);
         } else {
+          // 多个声明替换
           nodePath.replaceWithMultiple(importDeclarations);
         }
       },
